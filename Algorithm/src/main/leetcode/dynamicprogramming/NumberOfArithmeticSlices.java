71a319fa700b70b921f591d3e04420d8b35a30f9
sekics <sekics@zju.edu.cn>
2021-08-11 09:36:20 +0800

    modified

--- Algorithm/src/main/leetcode/dynamicprogramming/NumberOfArithmeticSlices.java
+++ Algorithm/src/main/leetcode/dynamicprogramming/NumberOfArithmeticSlices.java
@@ -7,17 +7,17 @@ public class NumberOfArithmeticSlices {
 
     public int numberOfArithmeticsSlices(int[] nums){
         int n = nums.length ;
-        Map<Integer,Integer>[] maps = new HashMap[n] ;
+        Map<Long,Integer>[] maps = new HashMap[n] ;
         for( int i = 0 ; i < n ; i ++ ){
-            maps[i] = new HashMap<Integer,Integer>() ;
+            maps[i] = new HashMap<Long,Integer>() ;
         }
         int ans = 0 ;
         for( int i = 0 ; i < n ; i ++ ){
             for( int j = 0 ; j < i ; j ++ ){
-                int diff = nums[i] - nums[j] ;
+                long diff = (long)nums[i] - (long)nums[j] ;
                 int cnt = maps[j].getOrDefault(diff,0) ;
                 ans += cnt ;
-                maps[i].put(diff,maps[i].getOrDefault(nums[i],0) + cnt + 1 ) ;
+                maps[i].put(diff,maps[i].getOrDefault(diff,0) + cnt + 1 ) ;
             }
         }
         return ans ;
